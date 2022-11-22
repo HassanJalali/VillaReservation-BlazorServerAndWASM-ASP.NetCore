94b15403b9074be4b5cfed191ac44a6ab71ee677
Hassan Jalali <mister.developer@yahoo.com>
2022-11-22 22:37:02 +0330

    HotelRoom
    add services for hotel room

---
+++ Business/Repository/HotelRoomRepository.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using Business.Mapper;
+using Business.Repository.IRepository;
+using DataAccess.Data;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Repository
+{
+    public class HotelRoomRepository : IHotelRoomRepository
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public HotelRoomRepository(ApplicationDbContext context, IMapper mapper)
+        {
+            _mapper = mapper;
+            _context = context;
+        }
+        public async Task<HotelRoomDTO> CreateHotelRoom(HotelRoomDTO hotelRoomDTO)
+        {
+            HotelRoom hotelRoom = _mapper.Map<HotelRoomDTO, HotelRoom>(hotelRoomDTO);
+            hotelRoom.CreatedDate = DateTime.Now;
+            hotelRoom.CreatedBy = "";
+            var addedHotelRoom = await _context.HotelRooms.AddAsync(hotelRoom);
+            await _context.SaveChangesAsync();
+            return _mapper.Map<HotelRoom,HotelRoomDTO>(addedHotelRoom.Entity);
+        }
+
+        public Task<int> DeleteHotelRoom(int roomId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<HotelRoomDTO>> GetAllHotelRooms()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HotelRoomDTO> GetHotelRoom(int roomId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HotelRoomDTO> IsRoomUnique(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HotelRoomDTO> UpdateHotelRoom(int roomId, HotelRoomDTO hotelRoomDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HiddenVilla_Server/Program.cs
@@ -0,0 +1,37 @@
+using Business.Repository.IRepository;
+using DataAccess.Data;
+using HiddenVilla_Server.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+IConfiguration Configuration = builder.Configuration;
+// Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+
+var app = builder.Build();
+
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
